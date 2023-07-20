ecc52bb87a05d2beb0fdc374252c581b1540541c
Samuel Joly <sjoly@fatfish.ca>
2023-07-19 21:37:07 -0400

    Fix deployment by setting base for deployment URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://samjol.github.io'
+  site: 'https://samjol.github.io',
+  base: '/portfolio',
 });
